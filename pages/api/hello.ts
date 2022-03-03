4389def5eb76ee738cc579556f3f125aad3eaab2
jules <jsoaresandrie@gmail.com>
2022-03-02 23:36:42 -0300

    Db integration with prisma - p6

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import prisma from 'lib/prisma'
+
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
+  id: number
   name: string
+  slug: string
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse<Data[]>
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const accounts = await prisma.account.findMany();
+  res.status(200).json(accounts)
 }
